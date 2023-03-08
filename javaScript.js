c4c87911570267aa6b6c5ecd1642c25264704b51
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-08 20:27:03 -0300

    Feat: make the grid adjust inside the wrapper, and be changeable

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,18 @@
 const gridCreate = document.getElementsByClassName("wrapper")[0];
+const divDimensions = ((gridCreate.clientWidth) / 16) -1.6;
 
 const pixDiv = document.createElement("div");
-pixDiv.style.width = "31.25px";
-pixDiv.style.height = "31.25px";
-//pixDiv.style.border = "1px solid black";
+pixDiv.style.width = `${divDimensions + .8}px`;
+pixDiv.style.height = `${divDimensions}px`;
+pixDiv.style.borderTop = "0.8px solid black";
+pixDiv.style.borderLeft = "0.8px solid black";
 pixDiv.style.margin = '0px';
-pixDiv.style.backgroundColor = 'lightgrey';
+//pixDiv.style.backgroundColor = 'lightgrey';
 
-const numInteractions = Math.floor(gridCreate.clientWidth / 31.25);
+const numInteractions = Math.floor(gridCreate.clientWidth / divDimensions);
 
-for (let i = 0; i < numInteractions; i++) {
-  for (let y = 0; y < numInteractions; y++) {
+for (let i = 0; i < 16; i++) {
+  for (let y = 0; y < 16; y++) {
     const clonePixDiv = pixDiv.cloneNode(true);
     gridCreate.appendChild(clonePixDiv);
   };
