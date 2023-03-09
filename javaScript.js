eec95f3c53eaddf58aaf57ad32d67b229d54fef1
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-09 12:16:11 -0300

    UpDate: update grid function to create the element itself

--- javaScript.js
+++ javaScript.js
@@ -2,15 +2,6 @@ const gridWrapper = document.getElementsByClassName("wrapper")[0];
 const gridSize = 16;
 const divDimensions = gridWrapper.clientWidth / 16;
 //-1.6 + 0.8
-const pixDiv = document.createElement("div");
-pixDiv.style.width = `${divDimensions}px`;
-pixDiv.style.height = `${divDimensions}px`;
-pixDiv.style.backgroundColor = "lightgrey";
-//pixDiv.style.borderTop = "0.8px solid black";
-//pixDiv.style.borderLeft = "0.8px solid black";
-pixDiv.style.margin = "0px";
-pixDiv.classList.add("div-clone");
-
 
 //Paiting the grid as the mouse goes over and is clicked down
 gridWrapper.addEventListener("mousemove", function (event) {
@@ -19,16 +10,25 @@ gridWrapper.addEventListener("mousemove", function (event) {
     if (target.classList.contains("div-clone")) {
       target.style.backgroundColor = "black";
     }
+  } else {
+    return;
   }
 });
 
-// Creating the div
-function gridCreate(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
-    for (let y = 0; y < gridSize; y++) {
-      const clonePixDiv = pixDiv.cloneNode(true);
-      gridWrapper.appendChild(clonePixDiv);
-    }
-  }
-}
-gridCreate(gridSize);
+// Creating the div grd with flexbox
+function gridCreate(gridSize, divDimensions) {
+
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const pixDiv = document.createElement("div");
+    pixDiv.style.width = `${divDimensions}px`;
+    pixDiv.style.height = `${divDimensions}px`;
+    pixDiv.style.backgroundColor = "lightgrey";
+    //pixDiv.style.borderTop = "0.8px solid black";
+    //pixDiv.style.borderLeft = "0.8px solid black";
+    pixDiv.style.margin = "0px";
+    pixDiv.classList.add("div-clone");
+    gridWrapper.appendChild(pixDiv);
+  };
+};
+
+gridCreate(gridSize, divDimensions);
