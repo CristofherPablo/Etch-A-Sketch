3fd3a02385d03baa81e8cedd4517a019bd23a994
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-09 12:06:03 -0300

    Feat: add conditin to paint the div only when mouse is over andclicked down

--- javaScript.js
+++ javaScript.js
@@ -1,37 +1,34 @@
-const gridCreate = document.getElementsByClassName("wrapper")[0];
-const isMouseDown = `false`;
-const divDimensions = (gridCreate.clientWidth / 16) - 1.6;
-
+const gridWrapper = document.getElementsByClassName("wrapper")[0];
+const gridSize = 16;
+const divDimensions = gridWrapper.clientWidth / 16;
+//-1.6 + 0.8
 const pixDiv = document.createElement("div");
-pixDiv.style.width = `${divDimensions + 0.8}px`;
+pixDiv.style.width = `${divDimensions}px`;
 pixDiv.style.height = `${divDimensions}px`;
-pixDiv.style.borderTop = "0.8px solid black";
-pixDiv.style.borderLeft = "0.8px solid black";
+pixDiv.style.backgroundColor = "lightgrey";
+//pixDiv.style.borderTop = "0.8px solid black";
+//pixDiv.style.borderLeft = "0.8px solid black";
 pixDiv.style.margin = "0px";
-pixDiv.classList.add('div-clone');
+pixDiv.classList.add("div-clone");
 
-gridCreate.addEventListener('mousedown', function(event) {
-    isMouseDown = `true`;
+
+//Paiting the grid as the mouse goes over and is clicked down
+gridWrapper.addEventListener("mousemove", function (event) {
+  if (event.buttons == 1) {
+    const target = event.target;
+    if (target.classList.contains("div-clone")) {
+      target.style.backgroundColor = "black";
+    }
+  }
 });
 
-gridCreate.addEventListener('mouseup', function(event) {
-    isMouseDown = `false`;
-});
-
-gridCreate.addEventListener('mousemove', function(event) {
-    if(isMouseDown){
-        const target = event.target;
-        if(target.classList.contains('div-clone')){
-            target.style.backgroundColor = 'black';
-        };
-    };
-});
-
-for (let i = 0; i < 16; i++) {
-  for (let y = 0; y < 16; y++) {
-    const clonePixDiv = pixDiv.cloneNode(true);
-    gridCreate.appendChild(clonePixDiv);
+// Creating the div
+function gridCreate(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let y = 0; y < gridSize; y++) {
+      const clonePixDiv = pixDiv.cloneNode(true);
+      gridWrapper.appendChild(clonePixDiv);
+    }
   }
 }
-
-//
+gridCreate(gridSize);
