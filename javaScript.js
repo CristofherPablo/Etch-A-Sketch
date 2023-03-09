095843484d134938bd5d938493c533f2270ff57f
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-08 21:22:51 -0300

    Feat: change the backgound color when mouse is over

--- javaScript.js
+++ javaScript.js
@@ -1,21 +1,37 @@
 const gridCreate = document.getElementsByClassName("wrapper")[0];
-const divDimensions = ((gridCreate.clientWidth) / 16) -1.6;
+const isMouseDown = `false`;
+const divDimensions = (gridCreate.clientWidth / 16) - 1.6;
 
 const pixDiv = document.createElement("div");
-pixDiv.style.width = `${divDimensions + .8}px`;
+pixDiv.style.width = `${divDimensions + 0.8}px`;
 pixDiv.style.height = `${divDimensions}px`;
 pixDiv.style.borderTop = "0.8px solid black";
 pixDiv.style.borderLeft = "0.8px solid black";
-pixDiv.style.margin = '0px';
-//pixDiv.style.backgroundColor = 'lightgrey';
+pixDiv.style.margin = "0px";
+pixDiv.classList.add('div-clone');
 
-const numInteractions = Math.floor(gridCreate.clientWidth / divDimensions);
+gridCreate.addEventListener('mousedown', function(event) {
+    isMouseDown = `true`;
+});
+
+gridCreate.addEventListener('mouseup', function(event) {
+    isMouseDown = `false`;
+});
+
+gridCreate.addEventListener('mousemove', function(event) {
+    if(isMouseDown){
+        const target = event.target;
+        if(target.classList.contains('div-clone')){
+            target.style.backgroundColor = 'black';
+        };
+    };
+});
 
 for (let i = 0; i < 16; i++) {
   for (let y = 0; y < 16; y++) {
     const clonePixDiv = pixDiv.cloneNode(true);
     gridCreate.appendChild(clonePixDiv);
-  };
-};
+  }
+}
 
-// 31.25px width and height
+//
